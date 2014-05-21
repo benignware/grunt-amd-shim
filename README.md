f831cb8fbe410e5ae445b7a2d919ff3fd3388a4e
rexblack <mail@benignware.com>
2014-05-21 15:51:32 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 grunt-amdshim
 =============
+Build a define wrapper around javascript libraries to support anonymous amd loading. 
+
+
+Purpose
+-------
+
+This grunt plugin provides a build task for making older libraries that do not include the define pattern compatible with amd loaders like requirejs by wrapping the code in an define call. 
+
+Also, most of the javascript libraries expose one or more global variables on the window-object. This can lead to version conflicts when the embedding document also requires those modules.
+The amdshim tasks controls this by saving and restoring previously set globals and exporting the reference loaded by amd. 
+
+To achieve this, the plugin provides the following options: 
+
+<table>
+  <tr>
+    <th>Name</th>
+    <th>Description</th>
+  </tr>
+  <tr>
+    <td>dep</td>
+    <td>An object containing dependencies as name/value pairs</td>
+  </tr>
+  <tr>
+    <td>global</td>
+    <td>An array containing the names of the exposed globals</td>
+  </tr>
+  <tr>
+    <td>exports</td>
+    <td>The name of the variable that should be exported by the module definition</td>
+  </tr>
+</table>
+
+Example
+-------
+Configure an amdshim for mediaelement.js
+
+```
+// Gruntfile.js
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    amdshim: {
+      mediaelement: {
+        src: 'src/lib/mediaelement-hls/build/mediaelement-and-player.js', 
+        dest: 'tmp/lib/mediaelement-hls/build/mediaelement-and-player.js', 
+        options: {
+          dep: {
+            jquery: '$'
+          }, 
+          global: ['mejs', 'MediaElementPlayer'], 
+          exports: 'mejs'
+        }
+      }
+    }
+  });
+}
+grunt.registerTask('default', ['amdshim:mediaelement']);
+```
+
+Execute the task from the command line:
+```
+grunt amdshim:mediaelement
+```
+
+Notes
+-----
+Note that this method may not work with javascript libraries that do not only expose their private variables to global access but also directly work with the global references to these.
+
+  
+ 
+
---
+++ tasks/amdshim.js
@@ -0,0 +1,70 @@
+module.exports = function(grunt) {
+    
+    grunt.registerMultiTask('amdshim', 'build an define wrapper around a js file', function() {
+       
+       grunt.log.writeln(['amdshim: ' + this.files.length]);
+        var options = this.options({
+          // defaults
+        });
+        grunt.log.writeln(['amdshim: ' + options.test]);
+        
+        
+        this.files.forEach(function(f) {
+          var src = f.src.filter(function(filepath) {
+            // Warn on and remove invalid source files (if nonull was set).
+            if (!grunt.file.exists(filepath)) {
+              grunt.log.warn('Source file ' + filepath + ' not found.');
+              return false;
+            } else {
+              return true;
+            }
+          });
+    
+          if (src.length === 0) {
+            grunt.log.warn('Destination ' + f.dest + ' not written because src files were empty.');
+            return;
+          }
+          
+          var output = "";
+          
+          
+          var depValues = [];
+          for (var x in options.dep) {
+            depValues.push(options.dep[x]);
+          }
+          var depKeys = Object.keys(options.dep);
+          
+          output+= "define(" + JSON.stringify(depKeys) + ", function(" + depValues.join(", ") + ") {\n\n";
+          
+          output+= "var _____globals = {};\n";
+          
+          output+= "_____globals._public = {\n";
+          for (var i = 0, global; global = options.global[i]; i++ ) {
+            output+= "\t" + global + ": window." + global + ", \n";
+          }
+          output+= "};\n";
+          
+          output+= grunt.file.read(f.src) + "\n";
+          
+          
+          output+= "_____globals._private = {\n";
+          for (var i = 0, global; global = options.global[i]; i++ ) {
+            output+= "\t" + global + ": window." + global + ", \n";
+          }
+          output+= "};\n";
+          
+          output+= "for (var x in _____globals._public) {\n";
+          output+= "\twindow[x] = _____globals._public[x];\n";
+          output+= "}\n";
+          
+          
+          output+= "return _____globals._private['" + options.exports + "'];\n";
+          output+= "});";
+          grunt.log.writeln(['amdshim: ' + f.dest]);
+          
+          grunt.file.write(f.dest, output);
+
+        });
+    });
+    
+};
